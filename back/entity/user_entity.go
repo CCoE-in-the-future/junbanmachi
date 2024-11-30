03aa25c723e94d212b82a1c276e423f3d4f85915
Kota.Chiya <0114bamitonton@gmail.com>
2024-11-30 05:14:16 +0000

    DynamoDBに格納するときjsonの記述が必要？

--- back/entity/user_entity.go
+++ back/entity/user_entity.go
@@ -3,11 +3,11 @@ package entity
 import "time"
 
 type User struct {
-	ID           string    
-	Name         string    
-	NumberPeople int       
-	WaitStatus   bool     
-	ArrivalTime  time.Time
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	NumberPeople int       `json:"numberPeople"`
+	WaitStatus   bool      `json:"waitStatus"`
+	ArrivalTime  time.Time `json:"arrivalTime"`
 }
 
 
